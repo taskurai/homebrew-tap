7526ff496073a26fe061cecc02ecbbadf900c96a
runner <runner@Mac-1687354643994.local>
2023-06-21 13:57:02 +0000

    Add taskurai formula update version 0.4.0

--- Formula/taskurai.rb
+++ Formula/taskurai.rb
@@ -4,21 +4,21 @@
 class Taskurai < Formula
 	desc "Taskurai - provides an easy to setup scalable platform to run background tasks or jobs. Taskurai provides a convenient way to offload workload to asynchronic tasks, resulting in more scalable and responsive systems."
 	homepage "https://taskurai.com"
-	version "0.3.3"
+	version "0.4.0"
 	license "commercial"
 
 	on_macos do
 	  if Hardware::CPU.intel?
-		url "https://get.taskurai.com/releases/0.3.3/taskurai_0.3.3_osx-x64.zip"
-		sha256 "20902d0dafd6d9f8e23f80f3954c4c75a603d0eb824873ad720821bfc7617d20"
+		url "https://get.taskurai.com/releases/0.4.0/taskurai_0.4.0_osx-x64.zip"
+		sha256 "e402381bdd1baabd1247469208f620452e071a05c1328e4a2b28ffafe17a05aa"
   
 		def install
 		  bin.install "taskurai"
 		end
 	  end
 	  if Hardware::CPU.arm?
-		url "https://get.taskurai.com/releases/0.3.3/taskurai_0.3.3_osx-arm64.zip"
-		sha256 "1d796c2eb06307303abf3713ccd8a198dfc3e4460ee17a98b5d054b557dceafa"
+		url "https://get.taskurai.com/releases/0.4.0/taskurai_0.4.0_osx-arm64.zip"
+		sha256 "da76a45338d4d8b19ac4323b0f4bad571e3df5491e504a1aeb3d7c70ed8757b5"
   
 		def install
 		  bin.install "taskurai"
@@ -28,16 +28,16 @@ class Taskurai < Formula
   
 	on_linux do
 	  if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-		url "https://get.taskurai.com/releases/0.3.3/taskurai_0.3.3_linux-arm64.zip"
-		sha256 "03dcf1592352d8b4e5d11ebfe4e85148df346920d96ba98b2622a2ae8d5d600c"
+		url "https://get.taskurai.com/releases/0.4.0/taskurai_0.4.0_linux-arm64.zip"
+		sha256 "6076a88e1c663d3749e904c0923c71b1b19fe06db438b3c378e02dc2fb7865c8"
   
 		def install
 		  bin.install "taskurai"
 		end
 	  end
 	  if Hardware::CPU.intel?
-		url "https://get.taskurai.com/releases/0.3.3/taskurai_0.3.3_linux-x64.zip"
-		sha256 "c61ac20a33e80c4a2b70506d84caea52a6567d55aea9195a716801505209373d"
+		url "https://get.taskurai.com/releases/0.4.0/taskurai_0.4.0_linux-x64.zip"
+		sha256 "0c306ff0f6e3ea2189f3098aab6dd061f9fcfcaaa8ec189ae0118f72b5b350d9"
   
 		def install
 		  bin.install "taskurai"
