5b888f58ac8e4d7a99991170ecb87c66ff954a53
runner <runner@Mac-1689866170311.local>
2023-07-20 15:19:42 +0000

    Add taskurai formula update version 1.3.2

--- Formula/taskurai.rb
+++ Formula/taskurai.rb
@@ -4,21 +4,21 @@
 class Taskurai < Formula
 	desc "Taskurai - provides an easy to setup scalable platform to run background tasks or jobs. Taskurai provides a convenient way to offload workload to asynchronic tasks, resulting in more scalable and responsive systems."
 	homepage "https://taskurai.com"
-	version "1.1.0"
+	version "1.3.2"
 	license "commercial"
 
 	on_macos do
 	  if Hardware::CPU.intel?
-		url "https://get.taskurai.com/releases/1.1.0/taskurai_1.1.0_osx-x64.zip"
-		sha256 "8cdc4007c0fc5bbb5bb803eded5b3afc15161272774d6b635594e98272f99782"
+		url "https://get.taskurai.com/releases/1.3.2/taskurai_1.3.2_osx-x64.zip"
+		sha256 ""
   
 		def install
 		  bin.install "taskurai"
 		end
 	  end
 	  if Hardware::CPU.arm?
-		url "https://get.taskurai.com/releases/1.1.0/taskurai_1.1.0_osx-arm64.zip"
-		sha256 "1076d61016218fd70f97a31649391e2ffacebbb37d8a97fd11c46281e37116bb"
+		url "https://get.taskurai.com/releases/1.3.2/taskurai_1.3.2_osx-arm64.zip"
+		sha256 ""
   
 		def install
 		  bin.install "taskurai"
@@ -28,16 +28,16 @@ class Taskurai < Formula
   
 	on_linux do
 	  if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-		url "https://get.taskurai.com/releases/1.1.0/taskurai_1.1.0_linux-arm64.zip"
-		sha256 "bea393694e9df1ccc9705088d8d5ecbbb65ae83170e0be868f9725a474f9f98a"
+		url "https://get.taskurai.com/releases/1.3.2/taskurai_1.3.2_linux-arm64.zip"
+		sha256 ""
   
 		def install
 		  bin.install "taskurai"
 		end
 	  end
 	  if Hardware::CPU.intel?
-		url "https://get.taskurai.com/releases/1.1.0/taskurai_1.1.0_linux-x64.zip"
-		sha256 "d0a6d33b9deb35c7f9a8d4b3fc76a2828260c4486801c8c19f69559b773bbecc"
+		url "https://get.taskurai.com/releases/1.3.2/taskurai_1.3.2_linux-x64.zip"
+		sha256 ""
   
 		def install
 		  bin.install "taskurai"
